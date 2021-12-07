81a05a2532521a6aae2db61f955a7317e51790ae
up201906682 <up201906682@fe.up.pt>
2021-12-07 09:15:33 +0000

    Start game and concede Button now have logic to be disabled when they should

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const $ = (selector) => document.querySelector(selector);
 
-$('#instructions').style.display = "none";
-$('#scoreboard').style.display = "none";
+$("#instructions").style.display = "none";
+$("#scoreboard").style.display = "none";
 
 const hideElem = (elem) => {
   $(elem).style.display = "none";
-}
+};
 
 const toggleBlockElem = (elem) => {
   prevDisplay = $(elem).style.display;
 
-  if (prevDisplay === "none")
-    $(elem).style.display = "block";
-  else
-    hideElem(elem);
-}
+  if (prevDisplay === "none") $(elem).style.display = "block";
+  else hideElem(elem);
+};
 
-const toggleInstructions = () => toggleBlockElem('#instructions');
-const toggleScoreboard = () => toggleBlockElem('#scoreboard');
+const toggleInstructions = () => toggleBlockElem("#instructions");
+const toggleScoreboard = () => toggleBlockElem("#scoreboard");
+const toggleConfig = () => toggleBlockElem("#configuration");
 
 const numHouses = $("input[name=numHousesRange]");
-numHouses.addEventListener('change', () => {
-  $('output[for=numHousesRange]').value = numHouses.value;
+numHouses.addEventListener("change", () => {
+  $("output[for=numHousesRange]").value = numHouses.value;
 });
 
 const numSeeds = $("input[name=numSeedsRange]");
-numSeeds.addEventListener('change', () => {
-  $('output[for=numSeedsRange]').value = numSeeds.value;
+numSeeds.addEventListener("change", () => {
+  $("output[for=numSeedsRange]").value = numSeeds.value;
 });
+
+const startGame = () => {
+  const concedeButton = $("button[id=concedeButton]");
+  const startButton = $("button[id=startButton]");
+  concedeButton.disabled = false;
+  startButton.disabled = true;
+  toggleConfig();
+};
+
+const concede = () => {
+  const concedeButton = $("button[id=concedeButton]");
+  const startButton = $("button[id=startButton]");
+  concedeButton.disabled = true;
+  startButton.disabled = false;
+  toggleConfig();
+};
