7c86a2e41412c9af191d4e0b351837e255d6803a
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-12-24 17:35:01 +0000

    Extracted Bot class from Game class

--- scripts/Game.js
+++ scripts/Game.js
@@ -6,32 +6,16 @@ class Game {
   play(playFirst, multiplayer, aiLevel) {
     this.currentPlayer = playFirst ? 1 : 2;
     this.multiplayer = multiplayer;
-    this.aiLevel = aiLevel;
+
+    if (!multiplayer)
+      this.bot = new Bot(aiLevel, this.boardController);
 
     if (playFirst) this.enablePlay();
     else this.aiTurn();
   }
 
   aiTurn() {
-    let houseIdx;
-    switch (this.aiLevel) {
-      case 'easy':
-        houseIdx = this.aiEasyTurn();
-        break;
-      case 'medium':
-        houseIdx = this.aiMediumTurn();
-        break;
-      case 'hard':
-        houseIdx = this.aiHardTurn();
-        break;
-      default:
-        console.log("Invalid AI Level");
-    }
-
-    const oldBoard = this.boardController.copy();
-
-    const playAgain = this.boardController.turn(houseIdx, 2);
-    this.boardController.updateSeeds(oldBoard);
+    const playAgain = this.bot.turn();
 
     const newScore = this.boardController.getScore(2);
     const scoreDiff = newScore - oldScore;
@@ -40,7 +24,6 @@ class Game {
         scoreDiff != 1 ? "points" : "point"
       } this round!`
     );
-
     this.updateScores(2);
 
     if (playAgain) {
@@ -52,77 +35,6 @@ class Game {
     this.currentPlayer = 1;
     if (this.isGameOver()) this.declareWinner();
     else this.enablePlay();
-  }
-
-  /**
-   * Chooses a random not empty house
-   */
-  aiEasyTurn() {
-    const possiblePlays = this.boardController.getPossiblePlays(2);
-
-    const choice = Math.floor(
-      Math.random() * possiblePlays.length
-    );
-  
-    return possiblePlays[choice];
-  }
-
-  /**
-   * Select a house that lets him play again
-   * If there are none, chooses randomly
-   */
-  aiMediumTurn() {
-    const possiblePlays = this.boardController.getPossiblePlays(2);
-    const currentBoard = this.boardController.copy();
-
-    for (let play of possiblePlays) {
-      if (this.boardController.turn(play, 2, currentBoard))
-        return play;
-    }
-
-    const randomChoice = Math.floor(
-      Math.random() * possiblePlays.length
-    );
-    return possiblePlays[randomChoice];
-  }
-
-  /**
-   * Select a house that lets him play again
-   * If there are none, chooses the house which gives him the most points
-   * Prioritizes a turn which gives no chance of playing to the other player
-   */
-  aiHardTurn() {
-    const possiblePlays = this.boardController.getPossiblePlays(2);
-    const board = this.boardController.copy();
-    const currentScore = this.boardController.getScore(2, board);
-
-    let replayable = false;
-    let max = 0, chosen = possiblePlays[0];
-
-    for (let play of possiblePlays) {
-      const playAgain = this.boardController.turn(play, 2, board);
-
-      if (this.boardController.isPlayerBoardEmpty(1))
-        return play;
-
-      const scoreDiff = this.boardController.getScore(board) - currentScore;
-
-      if (playAgain && !replayable) {
-        replayable = true;
-        max = scoreDiff;
-        chosen = play;
-        continue;
-      }
-
-      if (!playAgain && replayable) continue;
-
-      if (scoreDiff > max) {
-        max = scoreDiff;
-        chosen = play;
-      }
-    }
-
-    return chosen;
   }
 
   playerTurn(houseIdx) {
@@ -163,8 +75,10 @@ class Game {
     const oldBoard = this.boardController.copy();
     this.boardController.collectAllSeeds(this.currentPlayer === 1 ? 2 : 1);
     this.boardController.updateSeeds(oldBoard);
+
     this.updateScores(1);
     this.updateScores(2);
+
     const playerOneScore = this.boardController.getScore(1);
     const playerTwoScore = this.boardController.getScore(2);
 
