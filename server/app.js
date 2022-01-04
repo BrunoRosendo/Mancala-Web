04e9fdbb9e720c1c64a24ae5574a8c3a17f904ff
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-01-04 22:09:21 +0000

    Started node server

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(async (req, res) => {
+  res.write("Hello World\n");
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
