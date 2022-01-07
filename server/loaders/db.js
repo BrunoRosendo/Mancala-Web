1c7ae66279dd7821c3d1cbfcee0f55db8621dd87
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-01-07 01:22:16 +0000

    Replaced mongodb with sqlite :(

--- server/loaders/db.js
+++ server/loaders/db.js
@@ -1,19 +1,47 @@
-const { MongoClient } = require("mongodb");
+const sqlite3 = require("sqlite3").verbose();
 const config = require("../config/env");
 
-let client;
+let db;
 
-module.exports = async () => {
-  if (!client) {
+const loadDB = () => {
+  if (!db) {
     try {
-      client = new MongoClient(config.db_url);
-      await client.connect();
+      db = new sqlite3.Database(`./loaders/${config.db_name}.db`);
+      db.serialize(createTables);
     } catch (err) {
-      console.error("Failed in connecting to MongoDB server (aborting)", err);
+      console.error("Failed in connecting to sqlite database (aborting)", err);
       process.exit(1);
     }
-    
-    console.log("Successfully connected to MongoDB server");
+
+    console.log("Successfully connected to sqlite database");
   }
-  return client.db(config.db_name);
+  return db;
 }
+
+const createTables = () => {
+  db.run(`CREATE TABLE IF NOT EXISTS player(
+    nick TEXT PRIMARY KEY,
+    password TEXT NOT NULL)`
+  );
+
+  /*
+    We always need the whole player's side so it's not worth it
+    to separate between storage and pits. It's saved as JSON
+  */
+  db.run(`CREATE TABLE IF NOT EXISTS game(
+    id TEXT PRIMARY KEY,
+    playerOne TEXT NOT NULL,
+    playerTwo TEXT NOT NULL,
+    turn TEXT,
+    playerOneSide TEXT
+    playerTwoSide TEXT)`
+  );
+
+  db.run(`CREATE TABLE IF NOT EXISTS ranking(
+    player TEXT PRIMARY KEY,
+    victories INTEGER DEFAULT 0,
+    games INTEGER DEFAULT 0)`
+  );
+}
+
+module.exports = loadDB();
